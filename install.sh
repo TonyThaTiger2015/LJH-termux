8afae1d4ba72f333f5c7dc75ae70c67850cd8a4d
TonyThaTiger2015 <Leontodd15@outlook.com>
2026-04-10 02:45:41 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg update -y
+pkg install -y git
+
+curl -o $PREFIX/bin/smiler https://raw.githubusercontent.com/YOURNAME/smiler/main/smiler
+
+chmod +x $PREFIX/bin/smiler
+
+echo "Smiler installed! type: smiler"
